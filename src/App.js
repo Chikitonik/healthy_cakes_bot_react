7b259018f9fb1ac0b1a7eec60cc3f39743cdf3aa
Chikitonik <chikitonik@gmail.com>
2023-01-10 23:05:47 +0200

    netlify_url

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import { useEffect } from "react";
 import "./App.css";
 // this is after putting script in public\index.html
-const tg = window.Telegram.WebApp;
 
 function App() {
   useEffect(() => {
     tg.ready();
   }, []);
 
-  const onClose = () => {
-    tg.close();
-  };
-
-  return (
-    <div className="App">
-      <button>Close</button>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
