d41953869523bfff3ba9c3c57eff3383ad3f4e91
Chikitonik <chikitonik@gmail.com>
2023-01-12 16:56:24 +0200

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -4,57 +4,7 @@ import ProductItem from "../ProductItem/ProductItem";
 import { useTelegram } from "../../hooks/useTelegram";
 import { useCallback, useEffect } from "react";
 
-const products = [
-  {
-    id: "1",
-    title: "Muffins",
-    price: 5500,
-    description: "Very tasty!",
-    src: "https://raw.githubusercontent.com/Chikitonik/healthy_cakes_bot_react/main/src/assets/images/products/muffins.jpg",
-  },
-  // {
-  //   id: "2",
-  //   title: "Куртка",
-  //   price: 12000,
-  //   description: "Зеленого цвета, теплая",
-  // },
-  // {
-  //   id: "3",
-  //   title: "Джинсы 2",
-  //   price: 5000,
-  //   description: "Синего цвета, прямые",
-  // },
-  // {
-  //   id: "4",
-  //   title: "Куртка 8",
-  //   price: 122,
-  //   description: "Зеленого цвета, теплая",
-  // },
-  // {
-  //   id: "5",
-  //   title: "Джинсы 3",
-  //   price: 5000,
-  //   description: "Синего цвета, прямые",
-  // },
-  // {
-  //   id: "6",
-  //   title: "Куртка 7",
-  //   price: 600,
-  //   description: "Зеленого цвета, теплая",
-  // },
-  // {
-  //   id: "7",
-  //   title: "Джинсы 4",
-  //   price: 5500,
-  //   description: "Синего цвета, прямые",
-  // },
-  // {
-  //   id: "8",
-  //   title: "Куртка 5",
-  //   price: 12000,
-  //   description: "Зеленого цвета, теплая",
-  // },
-];
+import products_data from "../../data/products_data";
 
 const getTotalPrice = (items = []) => {
   return items.reduce((acc, item) => {
@@ -112,7 +62,7 @@ const ProductList = () => {
 
   return (
     <div className={"list"}>
-      {products.map((item) => (
+      {products_data.map((item) => (
         <ProductItem product={item} onAdd={onAdd} className={"item"} />
       ))}
     </div>
